24b2bcbde644ca63efe3eeac71929dc4de5574d9
Daniel Hirsch <daniel.hirsch@tngtech.com>
2021-07-08 18:20:43 +0200

    modifies traffic in network + adds sumo config file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Genetic-SUMO
+
+## Get Started
+It's recommended to use a conda environment with python 3.7. 
+
+### Run project
+Run our python starting point
+
+```
+python main.py
+```
+
+or use sumo-gui with our net config
+
+```
+sumo-gui 2x2.sumocfg
+```
+
+### Adapt network in netedit
+To configure the network run
+
+```
+netedit 2x2.net.xml 
+```
+
+### Download real-world examples
+Run
+
+```
+python $SUMO_HOME/tools/osmWebWizard.py
+```
+
+and choose whatever you want to download
--- main.py
+++ main.py
@@ -7,18 +7,21 @@ else:
     sys.exit("Please declare the environment variable 'SUMO_HOME'")
 from sumo_rl import SumoEnvironment
 from sumo_rl.util.gen_route import write_route_file
-
+import random
 
 if __name__ == '__main__':
     env = SumoEnvironment(net_file='2x2.net.xml',
                           route_file='2x2.rou.xml',
                           out_csv_name='a2c',
                           single_agent=True,
-                          use_gui=False,
+                          use_gui=True,
                           num_seconds=100000,
                           min_green=5,
                           max_depart_delay=0)
     env.reset()
-    print("Step1:")
-    print(env.step(3))
+
+    while True:
+        step = random.randint(0, 3)
+        print("Step:", step)
+        env.step(step)
 
