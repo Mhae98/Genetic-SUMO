49bf0aa9ea57ef6780b67daf9b679c29dd435b5b
sebastian <haugg@cip.ifi.lmu.de>
2021-08-03 11:25:43 +0200

    manual implementation of action selection

--- main.py
+++ main.py
@@ -7,7 +7,24 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("Please declare the environment variable 'SUMO_HOME'")
 from sumo_rl import SumoEnvironment
-from sumo_rl.util.gen_route import write_route_file
+
+
+def find_best_action(observation):
+    # mapped action(bzw phase):relevant obs_index
+    # phase_mapping = {0: [8, 9, 4, 5], 1: [6, 7, 10, 11], 2: [12, 13, 0, 1], 3: [2, 3, 14, 15]}
+    standing_mapping = {0: [5, 9], 1: [7, 11], 2: [1, 13], 3: [3, 15]}
+    time_mapping = {0: [4, 8], 1: [6, 10], 2: [0, 12], 3: [2, 14]}
+    min_map = dict()
+    for action in range(4):
+        # chooses phase if cars are standing on it
+        if max([observation[index] for index in standing_mapping[action]]) == 1:
+            return action
+        # finds to each action the shortest time since activation
+        min_map[action] = min([observation[index] for index in time_mapping[action]])
+    vals = list(min_map.values())
+    # choose the phase with the shortest time since activation
+    return vals.index(min(vals))
+
 
 if __name__ == '__main__':
     env = SumoEnvironment(net_file='nets/single/single.net.xml',
@@ -15,23 +32,14 @@ if __name__ == '__main__':
                           additional_file='nets/single/single.det.xml',
                           out_csv_name='a2c',
                           single_agent=True,
-                          use_gui=False,
+                          use_gui=True,
                           num_seconds=100000,
                           min_green=5,
                           max_depart_delay=0)
 
-    print(env.reset())
+    observation = env.reset()
+    done = False
 
-    while True:
-        user_in = input('Select action: ')
-        if user_in == 'S':
-            break
-        try:
-            action = float(user_in)
-            if action > 3 or action < 0:
-                raise ValueError
-        except ValueError:
-            action = random.randint(0, 3)
-        print(f'Selected action: {action}')
-        print(env.step(action)[0])
+    while not done:
+        observation, reward, done, _ = env.step(find_best_action(observation))
 
